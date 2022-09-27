f4f5a6c4ded5c8ec4bc9c2d38f11b0901df22904
Gol3vka <gol3vka@163.com>
2022-09-27 19:31:05 +0800

    Alias is optional, recv_info supports 'str'

--- demo.py
+++ demo.py
@@ -3,13 +3,12 @@
 '''demo.py: a demonstrate of the e-mail sending module.
 @Author: Golevka2001<gol3vka@163.com>
 @Created Date: 2022/05/16
-@Last Modified Date: 2022/09/26
+@Last Modified Date: 2022/09/27
 '''
 
 from email_sending_module import EmailSendingModule
 
 import os
-
 
 ''' Load configuration '''
 # Method 1: using an yaml format config file
@@ -22,11 +21,11 @@ email_1.config.load_from_file(config_path)
 sender_information = {
     'address': '123@abc.xyz',
     'password': '******',
-    'alias': 'Alice'
+    #'alias': 'Alice'  # 'alias 'can be deleted from this dict
 }
 receivers_information = {
     'address': ['456@def.xyz', '789@ghi.xyz'],
-    'alias': ['Bob', 'Carol']
+    'alias': ['Bob', '']  # match the 2 lists
 }
 mail = {'subject': '[NOTIFICATION]', 'body': 'Hello!'}
 server_information = {'address': 'smtp.abc.xyz', 'port': 25}
@@ -34,7 +33,6 @@ server_information = {'address': 'smtp.abc.xyz', 'port': 25}
 email_2 = EmailSendingModule()
 email_2.config.load_from_parameters(sender_information, receivers_information,
                                     mail, server_information)
-
 ''' Send e-mails '''
 result_1 = email_1.send_emails()
 print('Success:', result_1[0], '\nFailure:', result_1[1])
--- email_sending_module.py
+++ email_sending_module.py
@@ -3,7 +3,7 @@
 '''email_sending_module.py: a module used to send e-mails,\
     providing more convenience.
 @Author: Golevka2001<gol3vka@163.com>
-@Version: 2.0.0
+@Version: 2.1.1
 @Created Date: 2022/05/16
 @Last Modified Date: 2022/09/27
 '''
@@ -37,24 +37,25 @@ class EmailSendingModule:
     '''
 
     def __init__(self) -> None:
+
         class Configuration:
             '''Configuration of e-mail sending module.
 
             Vars:
-                sender (dict): ['address'(str), 'password'(str), 'alias'(str)]
-                receivers (dict): ['address'(str_list), 'alias'(str_list)]
-                mail (dict): ['subject'(str), 'body'(str)]
-                serve (dict): ['address'(str), 'port'(int)]
-                resend (dict): ['enable'(bool), 'times'(int),\
+                sender (dict): {'address'(str), 'password'(str), [OPTIONAL]'alias'(str)}
+                receivers (dict): {'address'(str or list), [OPTIONAL]'alias'(str or list)}
+                mail (dict): {'subject'(str), 'body'(str)}
+                serve (dict): {'address'(str), 'port'(int)}
+                [OPTIONAL]resend (dict): {'enable'(bool), 'times'(int),\
                     'enable_random_interval'(bool), 'min_interval'(int),\
-                        'max_interval'(int), 'fixed_interval'(int)]
+                        'max_interval'(int), 'fixed_interval'(int)}
 
             Functions:
                 load_from_file()
                 load_from_parameters()
             '''
 
-            def load_from_file(self, config_path) -> None:
+            def load_from_file(self, config_path: str) -> None:
                 '''Load configuration from a yaml file.
 
                 Args:
@@ -79,16 +80,25 @@ class EmailSendingModule:
                     else:
                         self.resend = {'enable': False}
 
-            def load_from_parameters(self,
-                                     sender_information,
-                                     receivers_information,
-                                     mail,
-                                     server_information,
-                                     resend_options={'enable': False}) -> None:
+            def load_from_parameters(
+                    self,
+                    sender_information: dict,
+                    receivers_information: dict,
+                    mail: dict,
+                    server_information: dict,
+                    resend_options: dict = {'enable': False}) -> None:
                 '''Load configuration from these parameters.
 
                 Args:
-                    see the'Vars' part in comment of 'class Configuration '
+                    sender (dict): {'address'(str), 'password'(str),\
+                        [OPTIONAL]'alias'(str)}
+                    receivers (dict): {'address'(str or list),\
+                        [OPTIONAL]'alias'(str or list)}
+                    mail (dict): {'subject'(str), 'body'(str)}
+                    serve (dict): {'address'(str), 'port'(int)}
+                    [OPTIONAL]resend (dict): {'enable'(bool), 'times'(int),\
+                        'enable_random_interval'(bool), 'min_interval'(int),\
+                            'max_interval'(int), 'fixed_interval'(int)}
                 '''
                 # assign:
                 self.sender = sender_information
@@ -114,43 +124,69 @@ class EmailSendingModule:
             else:
                 # fixed interval:
                 interval = self.config.resend['fixed_interval']
-
-            for i in range(self.config.resend['times']):
-                if self._send_helper(self.config.sender, self.config.receivers,
-                                     self.config.mail, self.config.server):
-                    count[0] += 1
-                else:
-                    count[1] += 1
-                time.sleep(interval)
+            times_ = self.config.resend['times']
         else:
-            # send e-mail only once:
+            times_ = 1
+
+        for i in range(times_):
             if self._send_helper(self.config.sender, self.config.receivers,
                                  self.config.mail, self.config.server):
-                count[0] += 1
+                count[0] += len(self.config.receivers['address'])
             else:
-                count[1] += 1
+                count[1] += len(self.config.receivers['address'])
+            if times_ > 1:
+                time.sleep(interval)
         return count  # count = [<success_count>, <failure_count>]
 
-    def _send_helper(self, sender, receivers, mail, server) -> bool:
+    def _send_helper(self, sender: dict, receivers: dict, mail: dict,
+                     server: dict) -> bool:
         '''Send e-mails to receivers in the list separately.
 
         Args:
-            sender (dict): ['address'(str), 'password'(str), 'alias'(str)]
-            receivers (dict): ['address'(str_list), 'alias'(str_list)]
+            sender (dict): ['address'(str), 'password'(str),\
+                [OPTIONAL]'alias'(str)]
+            receivers (dict): ['address'(str_list),\
+                [OPTIONAL]'alias'(str_list)]
             mail (dict): ['subject'(str), 'body'(str)]
             serve (dict): ['address'(str), 'port'(int)]
 
         Returns:
             bool: True - successful, False - failed
         '''
-        from_user = self._format_address(sender['address'], sender['alias'])
-        to_users_list = list()
-        if not len(receivers['address']) == len(receivers['alias']):
-            raise Exception('Address list and alias name list do not match!')
+        # format address of sender
+        if 'alias' in sender:
+            from_user = self._format_address(sender['address'],
+                                             sender['alias'])
         else:
-            for address_, alias_ in zip(receivers['address'], receivers['alias']):
-                to_users_list.append(self._format_address(address_, alias_))
+            from_user = self._format_address(sender['address'])
 
+        # format addresses of receivers
+        to_users_list = list()
+        if isinstance(receivers['address'], str):
+            # only one receiver:
+            if 'alias' in receivers:
+                to_users_list.append(
+                    self._format_address(receivers['address'],
+                                         receivers['alias']))
+            else:
+                to_users_list.append(self._format_address(
+                    receivers['address']))
+        elif isinstance(receivers['address'], list):
+            # only one or more than one receiver:
+            if 'alias' in receivers:
+                if not len(receivers['address']) == len(receivers['alias']):
+                    raise Exception(
+                        'Address list and alias name list do not match!')
+                else:
+                    for address_, alias_ in zip(receivers['address'],
+                                                receivers['alias']):
+                        to_users_list.append(
+                            self._format_address(address_, alias_))
+            else:
+                for address_ in receivers['address']:
+                    to_users_list.append(self._format_address(address_))
+
+        # compose e-mail:
         message = MIMEText(mail['body'], 'plain', 'utf-8')
         message['Subject'] = Header(mail['subject'], 'utf-8').encode()
         message['From'] = from_user
@@ -158,7 +194,8 @@ class EmailSendingModule:
 
         try:
             # send e-mails:
-            smtp = smtplib.SMTP_SSL(server['address'])
+            smtp = smtplib.SMTP_SSL(
+                server['address'])  # if SSL is not supported, delete '_SSL'
             smtp.connect(server['address'], server['port'])
             smtp.login(sender['address'], sender['password'])
             smtp.sendmail(from_user, to_users_list, message.as_string())
@@ -169,18 +206,18 @@ class EmailSendingModule:
             print(str(e))
             return False
 
-    def _format_address(self, address, alias) -> str:
+    def _format_address(self, address: str, alias: str = None) -> str:
         '''Format address like 'Alice<123@abc.xyz>'
 
         Args:
             address (str): user's address
-            alias (str): user's alias name
+            [OPTIONAL]alias (str): user's alias name
 
         Returns:
             str: formatted address
         '''
-        if isinstance(alias, str):  # in Python3: unicode is replaced by str
+        if isinstance(alias, str):
             return formataddr((Header(alias, 'utf-8').encode(),
-                              address))  # EXAMPLE: Alice<123@abc.xyz>
+                               address))  # EXAMPLE: Alice<123@abc.xyz>
         else:
             return address  # EXAMPLE: 123@abc.xyz
